9d0e545b779df668eb88c2798c550f3d814c6653
aravind811 <136822693+aravind811@users.noreply.github.com>
2023-06-19 19:14:38 +0530

    Update apply.js

--- javascript/apply.js
+++ javascript/apply.js
@@ -1,13 +1,13 @@
 const person = {
-    fullName: function(city, country) {
-      return this.firstName + " " + this.lastName + "," + city + "," + country;
+    fullName: function(ipl, country) {
+      return this.firstName + " " + this.lastName + "  played for" + city + "and" + country;
     }
   }
   
   const person1 = {
-    firstName:"John",
-    lastName: "Doe"
+    firstName:"virat",
+    lastName: "kohli"
   }
   
- console.log(person.fullName.call(person1, ["Oslo", "Norway"]));
+ console.log(person.fullName.call(person1, ["rcb", "india"]));
   
