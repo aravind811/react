0a7cca47eed19e734e94c04cca4b9029e062b083
aravind811 <136822693+aravind811@users.noreply.github.com>
2023-06-19 19:10:54 +0530

    Update event_loop.js

--- javascript/event_loop.js
+++ javascript/event_loop.js
@@ -1,17 +1,3 @@
-/*const seconds = new Date().getTime() / 1000;
-
-setTimeout(() => {
-  // prints out "2", meaning that the callback is not called immediately after 500 milliseconds.
-  console.log(`Ran after ${new Date().getTime() / 1000 - seconds} seconds`);
-}, 500);
-
-while (true) {
-  if (new Date().getTime() / 1000 - seconds >= 2) {
-    console.log("Good, looped for 2 seconds");
-    break;
-  }
-}*/
-
 const seconds = new Date().getTime() / 1000;
 const sec2=new Date().getTime() / 1000 - seconds
 
@@ -22,7 +8,7 @@ setTimeout(() => {
 
 while (true) {
   if (new Date().getTime() / 1000 - seconds >= 2) {
-    console.log("Good, looped for 2 seconds");
+    console.log("looped for 2 seconds");
     break;
   }
 }
